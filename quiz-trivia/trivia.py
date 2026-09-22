24d166c896f93bcb49e176315732166283aae0b8
Jocelin <jocelinbernal33@gmail.com>
2026-09-22 02:08:25 -0600

    Corrige estructura y agrega quiz-trivia correctamente

---
+++ quiz-trivia/trivia.py
@@ -0,0 +1,24 @@
+import requests
+import html
+
+
+def trivia_fetch(num):
+    url = f"https://opentdb.com/api.php?amount={num}"
+    response = requests.get(url)
+
+    trivia = response.json()
+    return trivia
+
+
+def main():
+    cantidad = int(input("¿Cuántas preguntas quieres? "))
+    trivia = trivia_fetch(cantidad)
+    print(trivia)
+
+
+if __name__ == "__main__":
+    main()
+
+
+for pregunta in trivia["results"]:
+print(pregunta["question"])
